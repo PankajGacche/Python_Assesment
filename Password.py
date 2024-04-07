017ecf9a61607f9dea9be0722ae78b68a122ce40
Pankaj <pankajgacche.sdet@gmail.com>
2024-04-08 00:08:14 +0530

    Assessment-1:Check Password Strength.

---
+++ Password.py
@@ -0,0 +1,33 @@
+def check_password_strength(password):
+        l, u, p, d = 0, 0, 0, 0
+
+        if(len(password)<8):
+            return False #Password strength is invalid
+    
+        for i in password: 
+            if (i.islower()):  #Counting lowercase alphabets
+                l+=1  
+
+            if (i.isupper()):   #Counting uppercase alphabets
+                 u+=1           
+
+            if (i.isdigit()):    #Counting digits
+                    d+=1  
+
+            if(i=='!'or i=='@' or i=='#' or i=='$' or i=='%' or i=='^' or i=='&' or i=='*'):  #Counting the mentioned special characters
+                    p+=1 
+
+        if (l>=1 and u>=1 and p>=1 and d>=1 and l+p+u+d==len(password)):
+            return True #Password strength is valid
+        else:
+            return False #Password strength is invalid
+            
+password_strength=check_password_strength(input("Please enter password: \n"))
+
+if (password_strength):
+    print("Password strength is valid.")
+else:
+     print("Password strength is invalid.")
+
+    
+    
